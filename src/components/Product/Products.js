4526417fe4bd7f527c1a1ba35f304fc30755356e
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-09-25 19:58:58 +0600

    style added

--- src/components/Product/Products.js
+++ src/components/Product/Products.js
@@ -3,11 +3,19 @@ import './Products.css'
 
 const Products = (props) => {
     console.log(props.product)
-    const {name,img,price,stock}= props.product;
+    const {name,img,price,stock,seller}= props.product;
     return (
-        <div>
-            <img src={img} alt="" />
+        <div className="product">
+           <div>
+           <img src={img} alt="" />
+           </div>
+
+            <div className="pro-info">
             <h4>{name}</h4>
+            <p><small>by-{seller}</small></p>
+            <p><small>price-{price}</small></p>
+            <p>only {stock} left in stock - order soon</p>
+            </div>
         </div>
     );
 };
