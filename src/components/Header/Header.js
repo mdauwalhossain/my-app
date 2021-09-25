3827bae3f6c5fd3c9c1066bfa2442876f62b7be0
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-09-25 10:26:12 +0600

    aaa

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>this is Header</h2>
+            
+            
+        </div>
+    );
+};
+
+
+export default Header;
